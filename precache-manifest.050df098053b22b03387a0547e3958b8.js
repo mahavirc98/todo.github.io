12439c930b8bf2737a82f8858eda839bc38cdffb
mahavir <mahavirc07@gmail.com>
2020-07-17 19:29:57 +0530

    Updates

---
+++ precache-manifest.050df098053b22b03387a0547e3958b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86bae43a9c0617659a83d87cc2af5d5",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a38ea71aa334ac5f3285",
+    "url": "/todo/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "ffa0ce1f5bcb805acb5f",
+    "url": "/todo/static/js/2.cbb27b25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.cbb27b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38ea71aa334ac5f3285",
+    "url": "/todo/static/js/main.9aafbb4d.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
